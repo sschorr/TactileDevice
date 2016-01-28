c2481cf6214fc66a9e356a93b1cfc13b685c92ae
sschorr <sschorr@stanford.edu>
2016-01-28 14:48:19 -0800

    fixed spring rate

--- include/c3dofdevice.h
+++ include/c3dofdevice.h
@@ -37,7 +37,7 @@
 // length of end effector (center to joint)
 #define L_EE 15
 // Spring torque of spring at parallel
-#define SPRING_TORQUE 0.5 //is the old one
+#define SPRING_TORQUE 0.05 //is the old one
 
 
 
