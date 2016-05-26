5ecf9dd115dd2125be70f3ab1f490cd36fb7c925
sschorr <sschorr@stanford.edu>
2016-05-25 17:18:38 -0700

    removing stickSlip code

--- src/haptics_thread.cpp
+++ src/haptics_thread.cpp
@@ -1075,16 +1075,14 @@ void haptics_thread::RenderTwoFriction()
     p_CommonData->p_frictionBox1->setLocalPos(0,.05, 0);
     p_CommonData->p_frictionBox2->setLocalPos(0,-.05, 0);
 
-    p_CommonData->p_frictionBox1->m_material->setStiffness(500);
+    p_CommonData->p_frictionBox1->m_material->setStiffness(300);
     p_CommonData->p_frictionBox1->m_material->setStaticFriction(0.4);
     p_CommonData->p_frictionBox1->m_material->setDynamicFriction(0.4*0.9);
-    p_CommonData->p_frictionBox2->m_material->setStickSlipStiffness(500); // N/m
 
 
-    p_CommonData->p_frictionBox2->m_material->setStiffness(500);
-    p_CommonData->p_frictionBox2->m_material->setStaticFriction(0.4);
-    p_CommonData->p_frictionBox2->m_material->setDynamicFriction(0.4*.9);
-    p_CommonData->p_frictionBox2->m_material->setStickSlipStiffness(1500); // N/m
+    p_CommonData->p_frictionBox2->m_material->setStiffness(300);
+    p_CommonData->p_frictionBox2->m_material->setStaticFriction(0.8);
+    p_CommonData->p_frictionBox2->m_material->setDynamicFriction(0.8*.9);
 
     world->addChild(p_CommonData->p_frictionBox1);
     world->addChild(p_CommonData->p_frictionBox2);
